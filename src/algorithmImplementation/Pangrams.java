b262dfd74a66712079471e421d688f9bfebfc8f9
Shalini Ravishankar <shalini.ravishankar@gmail.com>
2015-08-31 21:56:14 -0500

    Solution to Pangram

---
+++ src/algorithmImplementation/Pangrams.java
@@ -0,0 +1,43 @@
+package algorithmImplementation;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Pangrams {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    HashMap<Character, Boolean> resultMap = new HashMap<Character, Boolean>();
+    String text = input.nextLine();
+
+    String newText = text.replaceAll("\\s","").toLowerCase();
+    
+    int length = newText.length();
+    int count =0;
+    
+    for(char i = 'a'; i <= 'z' ; i++){
+      resultMap.put(i, false); 
+    }
+    
+    
+     for(int i=0; i<length; i++) {
+      
+      char temp = newText.charAt(i);
+      if(resultMap.containsKey(temp) && resultMap.get(temp) == false){
+        resultMap.put(temp, true);
+        count++;
+      }
+      
+    }
+    
+    if(count != 26) {
+      System.out.println("not pangram");
+    }
+    else {
+      System.out.println("pangram");
+    }
+    
+    
+  }
+
+}
