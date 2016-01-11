3eba19d20b4c8d4923c89a2c4052bba6c9ddb01d
Shalini Ravishankar <shalini.ravishankar@gmail.com>
2016-01-11 17:33:54 -0600

    Sorting problems added

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+public class BubbleSort {
+
+  public static void main(String[] args) {
+
+    int[] inputArray = { 4, 5, 6, 2, 3, 1 };
+
+    int[] result = bubbleSort(inputArray);
+    for (int i : result) {
+      System.out.print(i + " ");
+    }
+  }
+
+  /*
+   * Stable O(1) extra space O(n2) comparisons and swaps Adaptive: O(n) when
+   * nearly sorted
+   */
+  private static int[] bubbleSort(int[] inputArray) {
+
+    int n = inputArray.length;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - 1 - i; j++) {
+        if (inputArray[j] > inputArray[j + 1]) {
+          int temp = inputArray[j];
+          inputArray[j] = inputArray[j + 1];
+          inputArray[j +1] = temp;
+        }
+      }
+    }
+    return inputArray;
+  }
+
+}
