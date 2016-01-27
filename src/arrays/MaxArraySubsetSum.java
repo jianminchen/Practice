7bb6bda77838969983685bed455baa4fc40a7c2d
Shalini Ravishankar <shalini.ravishankar@gmail.com>
2016-01-26 22:23:03 -0600

    Signed-off-by: Shalini Ravishankar <shalini.ravishankar@gmail.com>

---
+++ src/arrays/MaxArraySubsetSum.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.ArrayList;
+
+public class MaxArraySubsetSum {
+
+  public static void main(String[] args) {
+
+    MaxArraySubsetSum ap = new MaxArraySubsetSum();
+    int[] temp = {1,-4,-5,-6,-7,-7,-3,0,20,10,13};
+     ap.findSubset(temp);
+  }
+
+  //Time O(N)
+  private void findSubset(int[] temp) {
+        
+    if (temp.length ==0) return;
+    
+    int n = temp.length;
+    int cur= temp[0];
+    int max = temp[0];
+    int x= 1, y = 1;
+    for (int i =1;i < n; i++) {
+    /*  cur = temp[i] + cur;
+      if (temp[i] > cur) {
+        cur = temp[i];
+        x = i+1;
+      }
+      
+      if (cur > max) {
+        max  =cur;
+        y = i+1;
+      }*/
+     
+      cur = Math.max(cur+temp[i], temp[i]);
+      max = Math.max(max, cur);
+    }
+        
+     System.out.println("Sum = "+max);
+   // System.out.println("Sum = "+max+" x = "+x+" y = "+y);
+  }
+
+}
---
+++ src/arrays/PrintDiagonal.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class PrintDiagonal {
+  
+  /*
+   * Give a N*N square matrix, return an array of its anti-diagonals.
+   */
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
